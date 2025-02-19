a673ca05bac91c1e1ece1c53ea59b8133fb40940
alex <mmclinks.ml@gmail.com>
2025-02-19 17:19:19 +0300

    Добавил README.md, тесты, docstrings и небольшие изменения.

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
+
+import config
 
 # Устанавливаем корневую папку проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,61 +25,60 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
 # Указываем кастомную модель пользователя
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
 
 # Установленные приложения
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'drf_yasg',  # Автодокументация API
-
-    'users',  # Наше приложение пользователей
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_yasg",  # Автодокументация API
+    "users",  # Наше приложение пользователей
 ]
 
 # Middleware (промежуточное ПО)
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # Основной файл маршрутов
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 # Настройки шаблонов
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
 # Настройки WSGI
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Подключение базы данных PostgreSQL
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql',
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("NAME"),
         "USER": os.getenv("USER"),
         "PASSWORD": os.getenv("PASSWORD"),
@@ -88,59 +90,59 @@ DATABASES = {
 # Валидаторы паролей Django
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Локализация
-LANGUAGE_CODE = 'ru-RU'  # Установим русский язык
+LANGUAGE_CODE = "ru-RU"  # Установим русский язык
 
-TIME_ZONE = 'UTC'  # Часовой пояс (можно поменять на 'Europe/Moscow')
+TIME_ZONE = "UTC"  # Часовой пояс (можно поменять на 'Europe/Moscow')
 
 USE_I18N = True
 USE_TZ = True
 
 # Настройки статики
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 if DEBUG:
     # В режиме отладки файлы берутся из папки `static`
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
     # В продакшене файлы собираются в `staticfiles/`
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Настройка поля по умолчанию для моделей Django
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URL-адрес для редиректа при попытке войти на защищённую страницу
-LOGIN_URL = '/send-otp/'
+LOGIN_URL = "/send-otp/"
 
 # Настройка логирования (если понадобится)
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/errors.log'),
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, "logs/errors.log"),
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
         },
     },
 }
--- users/serializers.py
+++ users/serializers.py
@@ -1,30 +1,84 @@
+"""
+Сериализаторы для работы с пользователями и валидации OTP-кодов.
+
+Модуль содержит сериализаторы для:
+- Сериализации данных пользователя (`UserSerializer`).
+- Генерации и валидации OTP-кодов для авторизации пользователя (`OTPSerializer`, `VerifyOTPSerializer`).
+"""
+
 from rest_framework import serializers
+
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели пользователя.
+
+    Используется для представления данных пользователя, таких как:
+    - username: имя пользователя.
+    - phone_number: номер телефона.
+    - invite_code: инвайт-код пользователя.
+    - invited_by: пользователь, который пригласил текущего.
+    """
+
     class Meta:
         model = User
-        fields = ['username', 'phone_number', 'invite_code', 'invited_by']
+        fields = ["username", "phone_number", "invite_code", "invited_by"]
 
 
 class OTPSerializer(serializers.Serializer):
+    """
+    Сериализатор для генерации OTP-кода.
+
+    Включает поле:
+    - phone_number: номер телефона пользователя, для которого нужно сгенерировать OTP.
+
+    Валидация:
+    - Если пользователь с таким номером не существует, он создается, а затем генерируется OTP-код.
+    """
+
     phone_number = serializers.CharField()
 
     def validate(self, data):
-        phone_number = data.get('phone_number')
+        """
+        Валидация и генерация OTP-кода.
+
+        :param data: данные сериализатора, включающие номер телефона.
+        :return: пользователь, для которого сгенерирован OTP-код.
+        :raises serializers.ValidationError: если пользователь не может быть создан.
+        """
+        phone_number = data.get("phone_number")
         user, created = User.objects.get_or_create(phone_number=phone_number)
         user.generate_otp_code()
         return user
 
 
 class VerifyOTPSerializer(serializers.Serializer):
+    """
+    Сериализатор для проверки OTP-кода.
+
+    Включает поля:
+    - phone_number: номер телефона пользователя, для которого нужно проверить OTP.
+    - otp_code: OTP-код, введенный пользователем.
+
+    Валидация:
+    - Проверяет, совпадает ли OTP-код с сохраненным кодом для данного пользователя.
+    """
+
     phone_number = serializers.CharField()
     otp_code = serializers.CharField()
 
     def validate(self, data):
-        phone_number = data.get('phone_number')
-        otp_code = data.get('otp_code')
+        """
+        Валидация OTP-кода.
+
+        :param data: данные сериализатора, включающие номер телефона и OTP-код.
+        :return: пользователь, если OTP-код правильный.
+        :raises serializers.ValidationError: если OTP-код неверный.
+        """
+        phone_number = data.get("phone_number")
+        otp_code = data.get("otp_code")
         user = User.objects.filter(phone_number=phone_number).first()
         if user and user.otp_code == otp_code:
             return user
